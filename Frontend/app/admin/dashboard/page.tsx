8419ea0da39ed397cc7a48cbdb5352acfee06b5f
Khushi jain <Khushijain.kj2612@gmail.com>
2025-11-20 21:12:03 +0530

    frontend

--- Frontend/app/admin/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-// app/admin/dashboard/page.tsx
-'use client';
-import { AdminDashboard } from '../../../src/components/AdminDashboard';
-
-export default function AdminDashboardPage() {
-  return <AdminDashboard />; // No props needed
-}
